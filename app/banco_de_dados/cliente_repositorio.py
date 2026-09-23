4a541629e68b4939169e1295f6f8d08a2d620569
Glauber Oliveira <glauberbatista88@gmail.com>
2026-09-23 11:24:51 -0300

    Implementando a base do banco de dados

---
+++ app/banco_de_dados/cliente_repositorio.py
@@ -0,0 +1,15 @@
+from app.banco_de_dados.local import BancoDeDadosLocal
+from app.modelos.cliente import Cliente
+
+
+class ClienteRepositorio:
+    def __init__(self, banco_de_dados: BancoDeDadosLocal):
+        self.bd = banco_de_dados
+
+    async def listar_clientes(self) -> list[Cliente]:
+        with self.bd.contectar() as conexao:
+            cursor = conexao.cursor()
+            cursor.execute("SELECT id, nome, email, telefone FROM clientes")
+            linhas = cursor.fetchall()
+            clientes = [Cliente(id_=linha[0], nome=linha[1], email=linha[2], telefone=linha[3]) for linha in linhas]
+        return clientes
